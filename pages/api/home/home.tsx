6033fb6c0b9722b2106821afd975e8a11cb11d11
BalkanCloud <160934587+BalkanCloud@users.noreply.github.com>
2024-12-04 11:00:33 +0100

    Update home.tsx

--- pages/api/home/home.tsx
+++ pages/api/home/home.tsx
@@ -303,7 +303,7 @@ const Home = ({ defaultModelId }: Props) => {
       }}
     >
       <Head>
-        <title>Chatbot Ollama</title>
+        <title>ChatGPT with Ollama</title>
         <meta name="description" content="ChatGPT but local." />
         <meta
           name="viewport"
